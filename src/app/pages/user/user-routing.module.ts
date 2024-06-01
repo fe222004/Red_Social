257bee254eaef9b6fd6ef07f3e05088788602aa6
Joss Balarezo <jfj.balarezo@yavirac.edu.ec>
2024-06-01 12:49:09 -0500

    edit y post

--- src/app/pages/user/user-routing.module.ts
+++ src/app/pages/user/user-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProfileComponent } from './profile/profile.component';
 import { EditProfileComponent } from './edit-profile/edit-profile.component';
+import { PostComponent } from './post/post.component';
 
 
 const routes: Routes = [
@@ -13,6 +14,10 @@ const routes: Routes = [
   {
     path:'edit',
     component: EditProfileComponent
+  },
+  {
+    path:'post',
+    component: PostComponent
   }
 ];
 
