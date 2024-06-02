093dfe57065915c23de621f8dcf3c5ef59244b73
niurka condor <nar.condor@yavirac.edu.ec>
2024-06-01 23:13:40 -0500

    Rutas Customer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,32 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CreatorComponent } from './pages/contentCreator/creator/creator.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { StoriesComponent } from './pages/components/stories/stories.component';
 
-const routes: Routes = 
-[
-  {
-    path:'customer',
-    loadChildren: () => import('./pages/customer-support/customer-support.module').then(m => m.CustomerSupportModule)
-  },
-  {
-     path:'p',
-    loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule)
-  },
-  {
-    path:'creator',
-    component: CreatorComponent
-  },
-  {
-    path:'',
-    component: DashboardComponent
-  },
-  {
-    path:'stories',
-    component: StoriesComponent
-  }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { StoriesComponent } from './components/stories/stories.component';
 
 const routes: Routes = [
 
@@ -14,12 +15,21 @@ const routes: Routes = [
   },
   {
     path: 'content',
-    loadChildren:() => import('./contents/contents.module').then(m => m.ContentsModule)
+    loadChildren: () => import('./contents/contents.module').then(m => m.ContentsModule)
   },
- {
-  path: 'inspector',
-  loadChildren:() => import('./inspector/inspector.module').then(m => m.InspectorModule)
- }
+  {
+    path: 'inspector',
+    loadChildren: () => import('./inspector/inspector.module').then(m => m.InspectorModule)
+  },
+  {
+    path:'customer',
+    loadChildren: () => import('./customer-support/customer-support.module').then(m => m.CustomerSupportModule)
+  },
+  {
+    path:'stories',
+    component: StoriesComponent
+  }
+
 ];
 
 @NgModule({
