0ec201447391572ccf312ae37daa1db1ed363e42
Fernanda Andrade <119636766+fe222004@users.noreply.github.com>
2024-06-07 20:14:46 -0500

    Update user.service.ts

--- src/user.service.ts
+++ src/user.service.ts
@@ -4,4 +4,8 @@ private redonly httpClient : HttpClient = inject(HttpClient)
 findUser(){
 this.httpClient.get('http:localhosth:3000/user'))
 }
+
+createUser(playload : any){
+this.httpClient.post('http:localhosth:3000/user',playload))
+}
 }
